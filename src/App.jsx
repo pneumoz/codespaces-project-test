16f371f34fbc78d7487b6c8ec13d366d7f64ffbe
Ajax Chen <kchan0850@gmail.com>
2026-05-08 02:17:17 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jay",
+  title: "Aspiring Creator",
+  email: "kchan0850@gmail.com",
+  gitHub: "pneumoz",
+  instagram: "pneumoz",
+  linkedIn: "Ajax Chen",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "ThunderboltSnak",
+  youTube: "ThunderboltSnake",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,21 +10,21 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/mathcat.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "A cat wearing a beret and glasses doing math equations on a whiteboard.";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a freshman at Stuyvesant High School in New York City. I have a passion for games and technology and am excited to continue learning and growing in the field of programming.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -44,8 +44,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "I do it for the love of the game";
 
 const About = () => {
   return (
